850456965df8c5fb94d9059c48bf9f1e6a7a420a
Nexla Developer <developer@nexla.com>
2025-09-14 08:16:11 +0530

    removed unwanted files

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { ChatPage } from './pages/ChatPage';
-import { CanvasTest } from './pages/CanvasTest';
+
 
 export const router = createBrowserRouter([
   { path: '/', element: <LandingPage /> },
   { path: '/chat', element: <ChatPage /> },
-  { path: '/canvasTest', element: <CanvasTest /> },
+  
 ]);
